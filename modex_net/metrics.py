a76b913284ca3828aeb9500897a6e30239d1f035
c.syranidou <c.syranidou@fz-juelich.de>
2021-07-09 08:00:27 +0200

    Use similaritymeasures package. Add normalized versions for all metrics.

--- modex_net/metrics.py
+++ modex_net/metrics.py
@@ -6,17 +6,25 @@
 import os
 import numpy as np
 from scipy.stats import wasserstein_distance, gaussian_kde
+import similaritymeasures
 
 
-def wasserstein(a, b):
+def _range(a, b):
     x_min = min([a.min(), b.min()])
     x_max = max([a.max(), b.max()])
-    x = np.linspace(x_min, x_max, 1000)
+    return np.linspace(x_min, x_max, 1000)
 
+def _points(x):
+    return np.array(list(zip(range(len(x)), x)))
+
+def wasserstein(a, b):
+    x = _range(a, b)
     pdf_a = gaussian_kde(a)
     pdf_b = gaussian_kde(b)
     return wasserstein_distance(pdf_a(x), pdf_b(x))
 
+def wasserstein_norm(a, b):
+    return wasserstein(a - a.mean(), b - b.mean())
 
 def bhattacharyya(a, b):
     """ Bhattacharyya distance between distributions (lists of floats).
@@ -24,9 +32,7 @@ def bhattacharyya(a, b):
     """
     if not len(a) == len(b):
         raise ValueError("a and b must be of the same size")
-    x_min = min([min(a), min(b)])
-    x_max = max([max(a), max(b)])
-    x = np.linspace(x_min, x_max, 1000)
+    x = _range(a, b)
 
     pdf_a = gaussian_kde(a)
     pdf_b = gaussian_kde(b)
@@ -35,51 +41,57 @@ def bhattacharyya(a, b):
     else:
         return -0.01
 
-
 def bhattacharyya_norm(a, b):
     return bhattacharyya(a - a.mean(), b - b.mean())
-
 
 def max_diff(a, b):
     return (a - b).abs().max()
 
+def max_diff_norm(a, b):
+    return max_diff(a - a.mean(), b - b.mean())
 
 def mape(a, b):
     from sklearn.metrics import mean_absolute_percentage_error
     return mean_absolute_percentage_error(a, b)
 
+def mape_norm(a, b):
+    return mape(a - a.mean(), b - b.mean())
 
 def euclidean(a, b):
     return np.linalg.norm(a - b)
 
+def euclidean_norm(a, b):
+    return euclidean(a - a.mean(), b - b.mean())
 
 def frechet(a, b):
-    import frechetdist
-    return frechetdist.frdist([range(len(a)), a], [range(len(b)), b])
+    return similaritymeasures.frechet_dist(_points(a), _points(b))
 
+def frechet_norm(a, b):
+    return frechet(a - a.mean(), b - b.mean())
 
 def dtw(a, b):
+    d, dd = similaritymeasures.dtw(_points(a), _points(b))
+    return d
 
-    # import module from given path: https://stackoverflow.com/questions/67631/how-to-import-a-module-given-the-full-path
-    import importlib.util
-    dtw_mod_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", "dtw", "dtw", "dtw.py"))
-    spec = importlib.util.spec_from_file_location("dtw", dtw_mod_path)
-    dtw_mod = importlib.util.module_from_spec(spec)
-    spec.loader.exec_module(dtw_mod)
-
-    return dtw_mod.accelerated_dtw(a.values, b.values, 'minkowski', p=1)[0]
-
+def dtw_norm(a, b):
+    return dtw(a - a.mean(), b - b.mean())
 
 def corr(a, b):
     return a.corr(b)
 
 
 metrics_dict = {'wasserstein': wasserstein,
+                'wasserstein_norm': wasserstein_norm,
                 'bhattacharyya': bhattacharyya,
                 'bhattacharyya_norm': bhattacharyya_norm,
                 'max_diff': max_diff,
+                'max_diff_norm': max_diff_norm,
                 'mape': mape,
+                'mape_norm': mape_norm,
                 'euclidean': euclidean,
+                'euclidean_norm': euclidean_norm,
                 'frechet': frechet,
+                'frechet_norm': frechet_norm,
                 'dtw': dtw,
+                'dtw_norm': dtw_norm,
                 'correlation': corr}
